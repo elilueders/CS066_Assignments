75e4433b50934bbad7bb060858b9b6b1a80279f1
elilueders <70550355+elilueders@users.noreply.github.com>
2022-03-05 21:07:47 -0600

    wiki crawler

--- wiki_crawler.py
+++ wiki_crawler.py
@@ -1,4 +1,5 @@
 import queue
+from re import I
 from urllib import response
 import requests
 from pythonds.basic import Queue
@@ -37,22 +38,59 @@ def get_page_links(wikitext):
             i += 1
     return list_of_linked_pages
 
-def get_wiki_page_results(wiki_title):
-    url = "https://en.wikipedia.org/w/rest.php/v1/page/" + wiki_title
+def request_wiki_page_results(wiki_title_req):
+    url = "https://en.wikipedia.org/w/rest.php/v1/page/" + wiki_title_req
     response = requests.get(url)
     return response.json()
 
+
 #Define your explore_wiki function here
-def explore_wiki(wiki_title, num_pages):
-    q_to_explore = Queue()
-    explored = set()
-    #test 
-    print(get_wiki_page_results(wiki_title)["source"][:num_pages])
+def explore_wiki(wiki_title, num_pages_requested):
+    cur_wiki_page_results = request_wiki_page_results(wiki_title)
+    q_to_explore = Queue() #1. initialize an empty _queue_ for keeping track of page titles that you haven't yet explored
+    set_explored = set() #2. initialize an empty _set_ <a href="#footnote2">[Note 2]</a> for keeping track of page titles that you have already explored
+    
+    # if "source" in cur_wiki_page_results:
+    # q_to_explore.enqueue("Sucker")
+    q_to_explore.enqueue(wiki_title) #3. Enqueue the starting page
+    
+    while len(set_explored) < num_pages_requested: #4. While you haven't yet explored enough pages
+        # x = q_to_explore.dequeue()
+        # y = request_wiki_page_results(x)
 
-mars_related = explore_wiki("Mars",1000)
+        cur_wiki_page_results = request_wiki_page_results(q_to_explore.dequeue()) #Dequeue the next page to explore
+         #Explore it by requesting the page from the Wikipedia API
+    
+        if "source" in cur_wiki_page_results:    #Check if the request worked <a href="#footnote3">[Note 3]</a>. If it did and you haven't already explored this page, then...
+            # print(True)
+            set_explored.add(cur_wiki_page_results["title"])    #add the current page to your set of explored pages
+            all_links = get_page_links(cur_wiki_page_results["source"])
+            
+            #test
+            print(cur_wiki_page_results["key"])
+            print(len(all_links),"explored: ",len(set_explored))
+            
+            for i in range(10):
+                if i < len(all_links):
+                    
+                    q_to_explore.enqueue(all_links[i]) #retrieve the first 10 outgoing links from the current page <a href="#footnote4">[Note 4]</a> and enqueue them so you can open and explore it on some future iteration of the loop
+        else: 
+            print("no source in link")
+    return set_explored
+
+
+# explore_wiki("Mars",10)
+# for x in request_wiki_page_results("File:Mars symbol (bold).svg"):
+#     print(x)
+
+
+
+mars_related = explore_wiki("Mars",150)
 print( mars_related )
-# print( len(mars_related) )
+print( len(mars_related) )
 
+"""
 cpu_related = explore_wiki("Central Processing Unit",10000)
 print( cpu_related )
 # print( len(cpu_related) )
+"""
