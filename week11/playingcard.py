843a969063b4b1d5e0d04891074e868ed9e37dc1
Your Name <you@example.com>
2022-04-04 01:15:47 -0500

    update week11

--- week11/playingcard.py
+++ week11/playingcard.py
@@ -26,6 +26,12 @@ class PlayingCard:
             return True
         else:
             return False
+
+    def __eq__(self, other):
+        return self.__value == other.__value
+    
+    def suit_match(self, other):
+        return self.__suit == other.__suit
         
 class Deck:
     
@@ -34,7 +40,12 @@ class Deck:
 
     def __repr__(self):
         return str(self.__card_list)
-        
+
+    def __lt__(self,other):
+        return len(self.__card_list) < other
+
+    def __ge__(self,other):
+        return len(self.__card_list) >= other
         
     def put_on_top(self,card):
         self.__card_list.append(card)
@@ -51,52 +62,101 @@ class Deck:
     def is_empty(self):
         return self.__card_list == []
 
+class Hand:
+
+    def __init__(self):
+        self.__card_list = []
+
+    def __repr__(self):
+        return str(self.__card_list)
+
+    def size(self):
+        return len(self.__card_list)
+    
+    def put_on_top(self,card):
+        self.__card_list.append(card)
+    
+    def remove_from_top(self):
+        if len(self.__card_list) == 0:
+            raise Exception("This deck has no cards left.")
+        else:
+            return self.__card_list.pop()
+
+    
+    def top_card(self):
+        return self.__card_list[-1]
+    def fourth_card(self):
+        return self.__card_list[-4]
+
+
+
+
+
 #Game
-#Game
-high_card_deck = Deck()
+stock_deck = Deck()
 
 #create each of the 52 playing cards and put them in the deck
 suits = ["♠","♣","♡","♢"]
 for s in suits:
     for v in range(2,15):
         curr_card = PlayingCard(v,s)
-        high_card_deck.put_on_top(curr_card)
+        stock_deck.put_on_top(curr_card)
         
 #look at the deck both before and after shuffling        
-print("Here's the pre-shuffled deck:",high_card_deck)
-high_card_deck.shuffle()
-print("Here's the deck after the shuffle:",high_card_deck)
+print("Here's the pre-shuffled deck:",stock_deck)
+stock_deck.shuffle()
+print("Here's the deck after the shuffle:",stock_deck)
 
-
-p1score = 0
-p2score = 0
-
+hand = Hand()
+waste_deck = Deck()
 
 #keep going until all cards are dealt out
-while not high_card_deck.is_empty():
+while not stock_deck.is_empty():
+    print("in while stock_deck not empty")
+    hand.put_on_top(stock_deck.remove_from_top())
+    # print(hand)
+    while hand.size() < 4:
+        print("hand has at less than 4")
+        hand.put_on_top(stock_deck.remove_from_top())
+    if hand.top_card() == hand.fourth_card():
+        print("value match")
+        print(hand)
+        for x in range(4):
+            waste_deck.put_on_top(hand.remove_from_top())
+        print(hand)
+    if hand.top_card().suit_match(hand.fourth_card()):
+        print("suit match")
+        print(hand)
+        temp = hand.remove_from_top()
+        waste_deck.put_on_top(hand.remove_from_top())
+        waste_deck.put_on_top(hand.remove_from_top())
+        hand.put_on_top(temp)
+        print(hand)
+print(stock_deck)
+print(hand)
+print(waste_deck)
+#     #draw a card for each player
+#     hand = stock_deck.remove_from_top()
+#     p2card = stock_deck.remove_from_top()
     
-    #draw a card for each player
-    p1card = high_card_deck.remove_from_top()
-    p2card = high_card_deck.remove_from_top()
+#     print("Player 1:",p1card,", Player 2:",p2card)
     
-    print("Player 1:",p1card,", Player 2:",p2card)
-    
-    #check which player wins this hand
-    if p1card < p2card:
-        p1score += 1
-        print("Player 1 wins this hand.")
-    elif p1card > p2card:
-        p2score += 1
-        print("Player 2 wins this hand.")
-    else:
-        print("This hand is a draw.")
+#     #check which player wins this hand
+#     if p1card < p2card:
+#         p1score += 1
+#         print("Player 1 wins this hand.")
+#     elif p1card > p2card:
+#         p2score += 1
+#         print("Player 2 wins this hand.")
+#     else:
+#         print("This hand is a draw.")
         
         
-#Figure out who wins and display the game-end message
-print("Player 1 score:",p1score,", Player 2 score:",p2score) 
-if p1score > p2score:
-    print("Player 1 wins the game!!!")
-elif p2score > p1score:
-    print("Player 2 wins the game!!!")
-else:
-    print("The game is a tie :(")
+# #Figure out who wins and display the game-end message
+# print("Player 1 score:",p1score,", Player 2 score:",p2score) 
+# if p1score > p2score:
+#     print("Player 1 wins the game!!!")
+# elif p2score > p1score:
+#     print("Player 2 wins the game!!!")
+# else:
+#     print("The game is a tie :(")
